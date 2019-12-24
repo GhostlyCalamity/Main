baacc4e7c8fbf069f7bcc5041b2e0981d98cbdb9
Serg <chsb2010@gmail.com>
2019-12-24 17:37:01 +0200

    all

---
+++ STM32F4-Discovery/frequency_counter/tim2-tim5-tim1-tim3-usb-mod/Debug/Middlewares/ST/STM32_USB_Device_Library/Class/CDC/Src/usbd_cdc.d
@@ -0,0 +1,119 @@
+Middlewares/ST/STM32_USB_Device_Library/Class/CDC/Src/usbd_cdc.o: \
+ ../Middlewares/ST/STM32_USB_Device_Library/Class/CDC/Src/usbd_cdc.c \
+ /home/vasyl/STM32/project/tim2-tim5-tim1-tim3-usb-mod/Middlewares/ST/STM32_USB_Device_Library/Class/CDC/Inc/usbd_cdc.h \
+ /home/vasyl/STM32/project/tim2-tim5-tim1-tim3-usb-mod/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_ioreq.h \
+ /home/vasyl/STM32/project/tim2-tim5-tim1-tim3-usb-mod/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_def.h \
+ /home/vasyl/STM32/project/tim2-tim5-tim1-tim3-usb-mod/Inc/usbd_conf.h \
+ /home/vasyl/STM32/project/tim2-tim5-tim1-tim3-usb-mod/Inc/main.h \
+ /home/vasyl/STM32/project/tim2-tim5-tim1-tim3-usb-mod/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h \
+ /home/vasyl/STM32/project/tim2-tim5-tim1-tim3-usb-mod/Inc/stm32f4xx_hal_conf.h \
+ /home/vasyl/STM32/project/tim2-tim5-tim1-tim3-usb-mod/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h \
+ /home/vasyl/STM32/project/tim2-tim5-tim1-tim3-usb-mod/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h \
+ /home/vasyl/STM32/project/tim2-tim5-tim1-tim3-usb-mod/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ /home/vasyl/STM32/project/tim2-tim5-tim1-tim3-usb-mod/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f407xx.h \
+ /home/vasyl/STM32/project/tim2-tim5-tim1-tim3-usb-mod/Drivers/CMSIS/Include/core_cm4.h \
+ /home/vasyl/STM32/project/tim2-tim5-tim1-tim3-usb-mod/Drivers/CMSIS/Include/cmsis_version.h \
+ /home/vasyl/STM32/project/tim2-tim5-tim1-tim3-usb-mod/Drivers/CMSIS/Include/cmsis_compiler.h \
+ /home/vasyl/STM32/project/tim2-tim5-tim1-tim3-usb-mod/Drivers/CMSIS/Include/cmsis_gcc.h \
+ /home/vasyl/STM32/project/tim2-tim5-tim1-tim3-usb-mod/Drivers/CMSIS/Include/mpu_armv7.h \
+ /home/vasyl/STM32/project/tim2-tim5-tim1-tim3-usb-mod/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ /home/vasyl/STM32/project/tim2-tim5-tim1-tim3-usb-mod/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ /home/vasyl/STM32/project/tim2-tim5-tim1-tim3-usb-mod/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h \
+ /home/vasyl/STM32/project/tim2-tim5-tim1-tim3-usb-mod/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_exti.h \
+ /home/vasyl/STM32/project/tim2-tim5-tim1-tim3-usb-mod/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h \
+ /home/vasyl/STM32/project/tim2-tim5-tim1-tim3-usb-mod/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h \
+ /home/vasyl/STM32/project/tim2-tim5-tim1-tim3-usb-mod/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h \
+ /home/vasyl/STM32/project/tim2-tim5-tim1-tim3-usb-mod/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h \
+ /home/vasyl/STM32/project/tim2-tim5-tim1-tim3-usb-mod/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h \
+ /home/vasyl/STM32/project/tim2-tim5-tim1-tim3-usb-mod/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h \
+ /home/vasyl/STM32/project/tim2-tim5-tim1-tim3-usb-mod/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h \
+ /home/vasyl/STM32/project/tim2-tim5-tim1-tim3-usb-mod/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h \
+ /home/vasyl/STM32/project/tim2-tim5-tim1-tim3-usb-mod/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
+ /home/vasyl/STM32/project/tim2-tim5-tim1-tim3-usb-mod/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h \
+ /home/vasyl/STM32/project/tim2-tim5-tim1-tim3-usb-mod/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim.h \
+ /home/vasyl/STM32/project/tim2-tim5-tim1-tim3-usb-mod/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim_ex.h \
+ /home/vasyl/STM32/project/tim2-tim5-tim1-tim3-usb-mod/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pcd.h \
+ /home/vasyl/STM32/project/tim2-tim5-tim1-tim3-usb-mod/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_ll_usb.h \
+ /home/vasyl/STM32/project/tim2-tim5-tim1-tim3-usb-mod/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pcd_ex.h \
+ /home/vasyl/STM32/project/tim2-tim5-tim1-tim3-usb-mod/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_core.h \
+ /home/vasyl/STM32/project/tim2-tim5-tim1-tim3-usb-mod/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_ioreq.h \
+ /home/vasyl/STM32/project/tim2-tim5-tim1-tim3-usb-mod/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_ctlreq.h \
+ /home/vasyl/STM32/project/tim2-tim5-tim1-tim3-usb-mod/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_ctlreq.h
+
+/home/vasyl/STM32/project/tim2-tim5-tim1-tim3-usb-mod/Middlewares/ST/STM32_USB_Device_Library/Class/CDC/Inc/usbd_cdc.h:
+
+/home/vasyl/STM32/project/tim2-tim5-tim1-tim3-usb-mod/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_ioreq.h:
+
+/home/vasyl/STM32/project/tim2-tim5-tim1-tim3-usb-mod/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_def.h:
+
+/home/vasyl/STM32/project/tim2-tim5-tim1-tim3-usb-mod/Inc/usbd_conf.h:
+
+/home/vasyl/STM32/project/tim2-tim5-tim1-tim3-usb-mod/Inc/main.h:
+
+/home/vasyl/STM32/project/tim2-tim5-tim1-tim3-usb-mod/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
+
+/home/vasyl/STM32/project/tim2-tim5-tim1-tim3-usb-mod/Inc/stm32f4xx_hal_conf.h:
+
+/home/vasyl/STM32/project/tim2-tim5-tim1-tim3-usb-mod/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h:
+
+/home/vasyl/STM32/project/tim2-tim5-tim1-tim3-usb-mod/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h:
+
+/home/vasyl/STM32/project/tim2-tim5-tim1-tim3-usb-mod/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+
+/home/vasyl/STM32/project/tim2-tim5-tim1-tim3-usb-mod/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f407xx.h:
+
+/home/vasyl/STM32/project/tim2-tim5-tim1-tim3-usb-mod/Drivers/CMSIS/Include/core_cm4.h:
+
+/home/vasyl/STM32/project/tim2-tim5-tim1-tim3-usb-mod/Drivers/CMSIS/Include/cmsis_version.h:
+
+/home/vasyl/STM32/project/tim2-tim5-tim1-tim3-usb-mod/Drivers/CMSIS/Include/cmsis_compiler.h:
+
+/home/vasyl/STM32/project/tim2-tim5-tim1-tim3-usb-mod/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+/home/vasyl/STM32/project/tim2-tim5-tim1-tim3-usb-mod/Drivers/CMSIS/Include/mpu_armv7.h:
+
+/home/vasyl/STM32/project/tim2-tim5-tim1-tim3-usb-mod/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+
+/home/vasyl/STM32/project/tim2-tim5-tim1-tim3-usb-mod/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+/home/vasyl/STM32/project/tim2-tim5-tim1-tim3-usb-mod/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h:
+
+/home/vasyl/STM32/project/tim2-tim5-tim1-tim3-usb-mod/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_exti.h:
+
+/home/vasyl/STM32/project/tim2-tim5-tim1-tim3-usb-mod/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h:
+
+/home/vasyl/STM32/project/tim2-tim5-tim1-tim3-usb-mod/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h:
+
+/home/vasyl/STM32/project/tim2-tim5-tim1-tim3-usb-mod/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h:
+
+/home/vasyl/STM32/project/tim2-tim5-tim1-tim3-usb-mod/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h:
+
+/home/vasyl/STM32/project/tim2-tim5-tim1-tim3-usb-mod/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h:
+
+/home/vasyl/STM32/project/tim2-tim5-tim1-tim3-usb-mod/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h:
+
+/home/vasyl/STM32/project/tim2-tim5-tim1-tim3-usb-mod/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h:
+
+/home/vasyl/STM32/project/tim2-tim5-tim1-tim3-usb-mod/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h:
+
+/home/vasyl/STM32/project/tim2-tim5-tim1-tim3-usb-mod/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h:
+
+/home/vasyl/STM32/project/tim2-tim5-tim1-tim3-usb-mod/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
+
+/home/vasyl/STM32/project/tim2-tim5-tim1-tim3-usb-mod/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim.h:
+
+/home/vasyl/STM32/project/tim2-tim5-tim1-tim3-usb-mod/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim_ex.h:
+
+/home/vasyl/STM32/project/tim2-tim5-tim1-tim3-usb-mod/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pcd.h:
+
+/home/vasyl/STM32/project/tim2-tim5-tim1-tim3-usb-mod/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_ll_usb.h:
+
+/home/vasyl/STM32/project/tim2-tim5-tim1-tim3-usb-mod/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pcd_ex.h:
+
+/home/vasyl/STM32/project/tim2-tim5-tim1-tim3-usb-mod/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_core.h:
+
+/home/vasyl/STM32/project/tim2-tim5-tim1-tim3-usb-mod/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_ioreq.h:
+
+/home/vasyl/STM32/project/tim2-tim5-tim1-tim3-usb-mod/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_ctlreq.h:
+
+/home/vasyl/STM32/project/tim2-tim5-tim1-tim3-usb-mod/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_ctlreq.h:
---
+++ STM32F4-Discovery/frequency_counter/tim2-tim5-tim1-tim3-usb-mod/Src/main.c
@@ -0,0 +1,517 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2019 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under Ultimate Liberty license
+  * SLA0044, the "License"; You may not use this file except in compliance with
+  * the License. You may obtain a copy of the License at:
+  *                             www.st.com/SLA0044
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "usb_device.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "usbd_cdc_if.h"
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+TIM_HandleTypeDef htim1;
+TIM_HandleTypeDef htim2;
+TIM_HandleTypeDef htim3;
+TIM_HandleTypeDef htim5;
+
+/* USER CODE BEGIN PV */
+char RawFreq[80];
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_TIM2_Init(void);
+static void MX_TIM5_Init(void);
+static void MX_TIM1_Init(void);
+static void MX_TIM3_Init(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_TIM2_Init();
+  MX_TIM5_Init();
+  MX_USB_DEVICE_Init();
+  MX_TIM1_Init();
+  MX_TIM3_Init();
+  /* USER CODE BEGIN 2 */
+  TIM2->CR1     |= TIM_CR1_CEN;//1: Counter enabled
+  TIM2->EGR |= TIM_EGR_UG;
+
+  TIM5->CR1     |= TIM_CR1_CEN;//1: Counter enabled
+  TIM5->EGR |= TIM_EGR_UG;
+
+  TIM1->CR1     |= TIM_CR1_CEN;//1: Counter enabled
+  TIM1->EGR |= TIM_EGR_UG;
+
+  TIM3->CR1     |= TIM_CR1_CEN;//1: Counter enabled
+  TIM3->EGR |= TIM_EGR_UG;
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  unsigned int freqTIM2Before=0;
+  unsigned int freqTIM2After=0;
+  unsigned int freqTIM2PerSecond=0;
+
+  unsigned int freqTIM5Before=0;
+  unsigned int freqTIM5After=0;
+  unsigned int freqTIM5PerSecond=0;
+
+  unsigned int freqTIM1Before=0;
+  unsigned int freqTIM1After=0;
+  unsigned int freqTIM1PerSecond=0;
+
+  unsigned int freqTIM3Before=0;
+  unsigned int freqTIM3After=0;
+  unsigned int freqTIM3PerSecond=0;
+  unsigned int GENcount=0;
+
+  while (1)
+  {
+    /* USER CODE END WHILE */
+		freqTIM2After=TIM2->CNT;
+		freqTIM2PerSecond=freqTIM2After-freqTIM2Before;
+		freqTIM2PerSecond-=freqTIM2PerSecond*0.00095;
+		freqTIM5After=TIM5->CNT;
+	    freqTIM5PerSecond=freqTIM5After-freqTIM5Before;
+	    freqTIM5PerSecond-=freqTIM5PerSecond*0.00095;
+		freqTIM1After=TIM1->CNT;
+	    freqTIM1PerSecond=freqTIM1After-freqTIM1Before;
+	    freqTIM1PerSecond-=freqTIM1PerSecond*0.00095;
+		freqTIM3After=TIM3->CNT;
+	    freqTIM3PerSecond=freqTIM3After-freqTIM3Before;
+	    freqTIM3PerSecond-=freqTIM3PerSecond*0.00095;
+
+		//sprintf(RawFreq, "%u \t %u \t %u \t %u \t %u \r\n", (unsigned int)(GENcount), (unsigned int)(freqTIM2PerSecond), (unsigned int)(TIM2->CNT), (unsigned int)(freqTIM5PerSecond), (unsigned int)(TIM5->CNT));
+		sprintf(RawFreq, "%u,%u,%u,%u,%u\r\n", GENcount, freqTIM2PerSecond, freqTIM5PerSecond, freqTIM1PerSecond, freqTIM3PerSecond);
+
+		if((TIM2->CNT>0 || TIM5->CNT>0)) CDC_Transmit_FS((uint8_t *)RawFreq, 80);
+
+		freqTIM2Before=freqTIM2After;
+		freqTIM5Before=freqTIM5After;
+		freqTIM1Before=freqTIM1After;
+		freqTIM3Before=freqTIM3After;
+
+		//if(TIM1->CNT+freqTIM1PerSecond>65000){
+			TIM1->EGR &= ~TIM_EGR_UG;
+			TIM1->EGR |= TIM_EGR_UG;
+			//freqTIM1Before=0;
+
+		//}
+		//if(TIM3->CNT+freqTIM1PerSecond>65000){
+			TIM3->EGR &= ~TIM_EGR_UG;
+			TIM3->EGR |= TIM_EGR_UG;
+			//freqTIM3Before=0;
+		//}
+		HAL_Delay(1000);
+		GENcount++;
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage 
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+  /** Initializes the CPU, AHB and APB busses clocks 
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = 4;
+  RCC_OscInitStruct.PLL.PLLN = 168;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 7;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Initializes the CPU, AHB and APB busses clocks 
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief TIM1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM1_Init(void)
+{
+
+  /* USER CODE BEGIN TIM1_Init 0 */
+
+  /* USER CODE END TIM1_Init 0 */
+
+  TIM_SlaveConfigTypeDef sSlaveConfig = {0};
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+  TIM_IC_InitTypeDef sConfigIC = {0};
+
+  /* USER CODE BEGIN TIM1_Init 1 */
+
+  /* USER CODE END TIM1_Init 1 */
+  htim1.Instance = TIM1;
+  htim1.Init.Prescaler = 0;
+  htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim1.Init.Period = 0xFFFF;
+  htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim1.Init.RepetitionCounter = 0;
+  htim1.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
+  if (HAL_TIM_Base_Init(&htim1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_TIM_IC_Init(&htim1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sSlaveConfig.SlaveMode = TIM_SLAVEMODE_EXTERNAL1;
+  sSlaveConfig.InputTrigger = TIM_TS_TI2FP2;
+  sSlaveConfig.TriggerPolarity = TIM_TRIGGERPOLARITY_RISING;
+  sSlaveConfig.TriggerFilter = 0;
+  if (HAL_TIM_SlaveConfigSynchro(&htim1, &sSlaveConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim1, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sConfigIC.ICPolarity = TIM_INPUTCHANNELPOLARITY_RISING;
+  sConfigIC.ICSelection = TIM_ICSELECTION_DIRECTTI;
+  sConfigIC.ICPrescaler = TIM_ICPSC_DIV1;
+  sConfigIC.ICFilter = 0;
+  if (HAL_TIM_IC_ConfigChannel(&htim1, &sConfigIC, TIM_CHANNEL_1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM1_Init 2 */
+
+  /* USER CODE END TIM1_Init 2 */
+
+}
+
+/**
+  * @brief TIM2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM2_Init(void)
+{
+
+  /* USER CODE BEGIN TIM2_Init 0 */
+
+  /* USER CODE END TIM2_Init 0 */
+
+  TIM_SlaveConfigTypeDef sSlaveConfig = {0};
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+  TIM_IC_InitTypeDef sConfigIC = {0};
+
+  /* USER CODE BEGIN TIM2_Init 1 */
+
+  /* USER CODE END TIM2_Init 1 */
+  htim2.Instance = TIM2;
+  htim2.Init.Prescaler = 0;
+  htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim2.Init.Period = 0xFFFFFFFF;
+  htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
+  if (HAL_TIM_Base_Init(&htim2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_TIM_IC_Init(&htim2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sSlaveConfig.SlaveMode = TIM_SLAVEMODE_EXTERNAL1;
+  sSlaveConfig.InputTrigger = TIM_TS_TI2FP2;
+  sSlaveConfig.TriggerPolarity = TIM_TRIGGERPOLARITY_RISING;
+  sSlaveConfig.TriggerFilter = 0;
+  if (HAL_TIM_SlaveConfigSynchro(&htim2, &sSlaveConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim2, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sConfigIC.ICPolarity = TIM_INPUTCHANNELPOLARITY_RISING;
+  sConfigIC.ICSelection = TIM_ICSELECTION_DIRECTTI;
+  sConfigIC.ICPrescaler = TIM_ICPSC_DIV1;
+  sConfigIC.ICFilter = 0;
+  if (HAL_TIM_IC_ConfigChannel(&htim2, &sConfigIC, TIM_CHANNEL_1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM2_Init 2 */
+
+  /* USER CODE END TIM2_Init 2 */
+
+}
+
+/**
+  * @brief TIM3 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM3_Init(void)
+{
+
+  /* USER CODE BEGIN TIM3_Init 0 */
+
+  /* USER CODE END TIM3_Init 0 */
+
+  TIM_SlaveConfigTypeDef sSlaveConfig = {0};
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+  TIM_IC_InitTypeDef sConfigIC = {0};
+
+  /* USER CODE BEGIN TIM3_Init 1 */
+
+  /* USER CODE END TIM3_Init 1 */
+  htim3.Instance = TIM3;
+  htim3.Init.Prescaler = 0;
+  htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim3.Init.Period = 0xFFFF;
+  htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim3.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
+  if (HAL_TIM_Base_Init(&htim3) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_TIM_IC_Init(&htim3) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sSlaveConfig.SlaveMode = TIM_SLAVEMODE_EXTERNAL1;
+  sSlaveConfig.InputTrigger = TIM_TS_TI2FP2;
+  sSlaveConfig.TriggerPolarity = TIM_TRIGGERPOLARITY_RISING;
+  sSlaveConfig.TriggerFilter = 0;
+  if (HAL_TIM_SlaveConfigSynchro(&htim3, &sSlaveConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim3, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sConfigIC.ICPolarity = TIM_INPUTCHANNELPOLARITY_RISING;
+  sConfigIC.ICSelection = TIM_ICSELECTION_DIRECTTI;
+  sConfigIC.ICPrescaler = TIM_ICPSC_DIV1;
+  sConfigIC.ICFilter = 0;
+  if (HAL_TIM_IC_ConfigChannel(&htim3, &sConfigIC, TIM_CHANNEL_1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM3_Init 2 */
+
+  /* USER CODE END TIM3_Init 2 */
+
+}
+
+/**
+  * @brief TIM5 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM5_Init(void)
+{
+
+  /* USER CODE BEGIN TIM5_Init 0 */
+
+  /* USER CODE END TIM5_Init 0 */
+
+  TIM_SlaveConfigTypeDef sSlaveConfig = {0};
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+  TIM_IC_InitTypeDef sConfigIC = {0};
+
+  /* USER CODE BEGIN TIM5_Init 1 */
+
+  /* USER CODE END TIM5_Init 1 */
+  htim5.Instance = TIM5;
+  htim5.Init.Prescaler = 0;
+  htim5.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim5.Init.Period = 0xFFFFFFFF;
+  htim5.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim5.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
+  if (HAL_TIM_Base_Init(&htim5) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_TIM_IC_Init(&htim5) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sSlaveConfig.SlaveMode = TIM_SLAVEMODE_EXTERNAL1;
+  sSlaveConfig.InputTrigger = TIM_TS_TI1FP1;
+  sSlaveConfig.TriggerPolarity = TIM_TRIGGERPOLARITY_RISING;
+  sSlaveConfig.TriggerFilter = 0;
+  if (HAL_TIM_SlaveConfigSynchro(&htim5, &sSlaveConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim5, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sConfigIC.ICPolarity = TIM_INPUTCHANNELPOLARITY_RISING;
+  sConfigIC.ICSelection = TIM_ICSELECTION_DIRECTTI;
+  sConfigIC.ICPrescaler = TIM_ICPSC_DIV1;
+  sConfigIC.ICFilter = 0;
+  if (HAL_TIM_IC_ConfigChannel(&htim5, &sConfigIC, TIM_CHANNEL_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM5_Init 2 */
+
+  /* USER CODE END TIM5_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOH_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOE_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{ 
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
