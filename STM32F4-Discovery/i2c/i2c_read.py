de279f7b95eff7d4e8139b6469d5d188cfd01862
Serg <chsb2010@gmail.com>
2019-12-26 12:50:22 +0200

    i2c read 2

--- STM32F4-Discovery/i2c/i2c_read.py
+++ STM32F4-Discovery/i2c/i2c_read.py
@@ -4,7 +4,7 @@ import time
  
 bus = smbus.SMBus(1)
 address = 0x09
-
+GenCount=0
 #def write(value):
 #        bus.write_byte_data(address, 0, value)
 #        return -1
@@ -13,9 +13,11 @@ while (1):
   #  write(0x10)
   time.sleep(1)
   #LBS = bus.read_byte_data(address,10)
-  data = bus.read_i2c_block_data(0x09, 0x10, 4)
+  data = bus.read_i2c_block_data(0x09, 0x10, 8)
   data1 = (data[0]<<24) + (data[1]<<16) + (data[2]<<8) + data[3]
-  print data1
+  data2 = (data[4]<<24) + (data[5]<<16) + (data[6]<<8) + data[7]
+  print GenCount, "\t", data1, "\t", data2
+  GenCount=GenCount+1
   #print LBS
    
     
